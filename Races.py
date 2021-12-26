420b2cb9cdb3e114127077d0ea87779f02e8564a
Robin Kelsick II <robinkelsickii@Mollies-MacBook-Pro.local>
2021-12-26 18:56:14 -0500

    added race class with ability modifier

--- Races.py
+++ Races.py
@@ -1,15 +1,47 @@
 
 
+
 class Races: 
+
+    #constructor for race
     def __init__(self, race):
         self.race = race
 
-dragonborn = Races("DragonBorn")
-dwarf = Races("Dwarf")
-elf = Races("Elf")
-gnome = Races("Gnome")
-halfElf = Races("Half-Elf")
-hafling = Races("Hafling")
-halfOrc = Races("Half-Orc")
-human = Races("Human")
-tiefling = Races("Tiefling")
+    #gets the race
+    def raceName(self):
+        return self.race
+
+    #ability modifiers dependant on race
+    def abilityModifier(self):
+        if self.race == 'dragonborn':
+            str = Strength + 2
+            cha = Charisma + 1
+        elif self.race == 'dwarf':
+            con = Constitution + 2 
+        elif self.race == 'elf':
+            dex = Dexterity + 2
+        elif self.race == 'gnome':
+            intel = Intelligence + 2 
+        elif self.race == 'half-elf':
+            cha = Charisma + 2 
+        elif self.race == 'halfling':
+            dex = Dexterity + 2
+        elif self.race == 'half-orc':
+            str = Strength + 2
+            con = Constitution + 1 
+        elif self.race == 'human':
+            str = Strength + 1
+            dex = Dexterity + 1
+            con = Constitution + 1
+            wis = Wisdom + 1
+            intel = Intelligence + 1
+            cha = Charaisma + 1
+        elif self.race == 'tiefling':
+            cha = Charisma + 2 
+            intel = Intelligence + 1
+            
+            
+        
+
+    
+    

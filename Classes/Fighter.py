0e26b153bc8cf1938cdf5149292a22c932814168
Ticia D <84279128+TiciaD@users.noreply.github.com>
2021-12-27 17:26:53 -0500

    refine Fighter and Rogue class

--- Classes/Fighter.py
+++ Classes/Fighter.py
@@ -2,6 +2,20 @@ import Character
 
 
 class Fighter(Character):
-    def __init__(self, first, last, alignment):
+    def __init__(self):
         self.hitPoints = 10  # 10HP per level
         # maxHitPoints = level * 10;
+        self.charClass = 'Fighter'
+
+    # Attack Method
+    def attack(self, target):
+        # attacks roll is increased by 1 for every level instead of every other level?
+        attackRoll = 11
+        if attackRoll == 20:
+            target.hitPoints = target.hitPoints - 2
+            print('critical hit')
+        if attackRoll >= target.ac:
+            target.hitPoints = target.hitPoints - 1
+            print('hit')
+        else:
+            print('fail')
--- Classes/Rogue.py
+++ Classes/Rogue.py
@@ -2,8 +2,19 @@ import Character
 
 
 class Rogue(Character):
-    def __init__(self, first, last, alignment):
-        self.alignment = 'Neutral' if alignment == 0 else 'Evil'
-        # if attackRoll == 20
-        #   target.hitPoints = target.hitPoints - 2 * 3
+    def __init__(self, alignment):
         # alignment CANNOT be Good
+        self.alignment = 'Neutral' if alignment == 0 else 'Evil'
+        self.charClass = 'Rogue'
+
+    def attack(self, target):
+        # attackRoll = randrange(1, 21, 2)
+        attackRoll = 11
+        if attackRoll == 20:
+            target.hitPoints = target.hitPoints - 2 * 3
+            print('critical hit')
+        if attackRoll >= target.ac:
+            target.hitPoints = target.hitPoints - 1
+            print('hit')
+        else:
+            print('fail')

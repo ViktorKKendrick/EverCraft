374150b1d62aa5c18e775d568bdef2f6f2ad2f8b
Ticia D <84279128+TiciaD@users.noreply.github.com>
2021-12-28 12:37:45 -0500

    fixed Fighter file issues

--- Classes/Fighter.py
+++ Classes/Fighter.py
@@ -5,13 +5,10 @@ from Modifiers import *
 class Fighter(Character):
     def __init__(self, Strength, level):
         self.hitPoints = 10  # 10HP per level
-        # maxHitPoints = level * 10;
         self.charClass = 'Fighter'
-        self.attRollMod = modifiers[Strength]
 
     # Attack Method for Fighter
     def attack(self, target, level):
-        # attacks roll is increased by 1 for every level instead of every other level?
         attackRoll = 11 + self.attRollMod + level
         damage = ((1 + self.attMod) * 2) if ((1 + self.attMod)*2 > 1) else 1
         if attackRoll == 20:
@@ -31,3 +28,10 @@ class Fighter(Character):
                 print('target is already dead, attack someone else you murderer!')
         else:
             print('fail')
+
+    # LevelUp Method
+    def levelUp(self):
+        self.level += 1
+        self.hitPoints += (10 + modifiers[self.Constitution])
+        # attacks roll is increased by 1 for every level instead of every other level
+        self.attRollMod += 1
